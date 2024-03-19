6dccaf0910aa22596c609be7f41da6ba2cfdc184
Steven M. Caruso <115181289+smcarustwo@users.noreply.github.com>
2024-03-19 12:28:46 -0400

    console interface

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,92 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+enum Sender {
+  console = "console",
+  system = "system",
+  error = "error",
+  user = "user"
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let terminal: HTMLElement = document.querySelector(".terminal")!
+
+function print(message: string, sender: Sender): void {
+
+  let previousLine = terminal.children[terminal.children.length - 1]
+  if (previousLine !== undefined && previousLine.classList.contains("prompt")) {
+    previousLine.remove()
+  }
+
+  let newLine: HTMLElement = document.createElement("div")
+  newLine.classList.add("terminal-line")
+
+  let senderCol: HTMLElement = document.createElement("div")
+  senderCol.classList.add("sender")
+  senderCol.classList.add(sender)
+  senderCol.innerText = sender
+
+  let messageCol: HTMLElement = document.createElement("div")
+  messageCol.classList.add("message")
+  messageCol.innerText = message
+
+  newLine.append(senderCol, messageCol)
+  terminal.appendChild(newLine)
+  
+  prompt()
+  window.scrollTo({top: window.innerHeight, behavior: 'smooth'})
+
+}
+
+function prompt(): void {
+
+  let newLine: HTMLElement = document.createElement("div")
+  newLine.classList.add("terminal-line")
+  newLine.classList.add("prompt")
+
+  let senderCol: HTMLElement = document.createElement("div")
+  senderCol.classList.add("sender")
+  senderCol.classList.add("user")
+  senderCol.innerText = ">_"
+
+  let messageCol: HTMLElement = document.createElement("div")
+  messageCol.classList.add("message")
+  let textbox: HTMLElement = document.createElement("textarea")
+  textbox.setAttribute("rows", "1")
+  textbox.setAttribute("placeholder", "prompt")
+  messageCol.appendChild(textbox)
+
+  newLine.append(senderCol, messageCol)
+  terminal.appendChild(newLine)
+  
+  textbox.addEventListener('input', (event: Event) => {
+    const target = event.target as HTMLTextAreaElement;
+    target.style.height = 'auto'
+    target.style.height = `${target.scrollHeight}px`
+  })
+  
+  textbox.addEventListener('keydown', (event: KeyboardEvent) => {
+    // Check if Enter is pressed without Shift
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault(); // Prevent the default action (new line)
+      const target = event.target as HTMLTextAreaElement;
+      let message: string = target.value
+      newLine.remove()
+      print(message, Sender.user)
+      process(message)
+    }
+  })
+
+}
+
+function process(message: string): void {
+
+  switch (message) {
+    case "clear":
+      terminal.innerHTML = ""
+      prompt()
+  }
+
+}
+
+// // // // // // // //
+
+print("Front-end client script is running.", Sender.system)
