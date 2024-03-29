499c1ee3e7f43dd821a2509c6bf612d0a23ca3c7
Steven M. Caruso <115181289+smcarustwo@users.noreply.github.com>
2024-03-29 18:21:58 -0400

    cards selection interface

--- src/gui.ts
+++ src/gui.ts
@@ -1,5 +1,7 @@
 import "./gui-style.css"
 import {App} from "./app"
+import { CardChoiceInterface } from "./cards"
 import { RefinementInterface } from "./refinement"
 
+const cardChoiceInterface: CardChoiceInterface = new CardChoiceInterface()
 const refinementInterface: RefinementInterface = new RefinementInterface(new App())
